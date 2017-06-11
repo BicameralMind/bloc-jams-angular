1007acf5deb400ff56df99fbb9b00f1953ca2b1c
Lauren Soni <Buzz@Laurens-iMac.home>
2017-06-11 11:56:39 -0400

    checkpoint10

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};
          
         var currentAlbum = Fixtures.getAlbum();
@@ -25,6 +25,12 @@
                 currentBuzzObject = new buzz.sound(song.audioUrl, {
                     formats: ['mp3'],
                     preload: true
+                });
+            
+                currentBuzzObject.bind('timeupdate', function() {
+                    $rootScope.$apply(function() {
+                        SongPlayer.currentTime = currentBuzzObject.getTime();
+                    });
                 });
             
                 SongPlayer.currentSong = song;
@@ -56,6 +62,42 @@
         */ 
          
         SongPlayer.currentSong = null;
+         
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+         
+        SongPlayer.currentTime = null;
+         
+        /**
+        * @desc The volume of the song player
+        * @type {Number}
+        */
+        SongPlayer.volume = 75;
+         
+        /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+         
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
+         
+        /**
+        * @function setVolume
+        * @desc Set volume of the song player
+        * @param {Number} volume
+        */
+        SongPlayer.setVolume = function(volume) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setVolume(volume)
+            }
+        }; 
          
         SongPlayer.play = function(song) {
             song = song || SongPlayer.currentSong;
@@ -110,6 +152,6 @@
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', ['Fixtures', SongPlayer]);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
 
